52d0ac736558c557618aa6fe1a6fc88e7019f5dd
Eric Wiener <ericwiener3@gmail.com>
2021-04-05 13:12:39 -0400

    REFACTOR: moved train.ipynb to root directory

--- src/pcd_de_noising/__init__.py
+++ src/pcd_de_noising/__init__.py
@@ -14,3 +14,6 @@ except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del version, PackageNotFoundError
+
+from .model import WeatherNet
+from .pcd_dataset import PCDDataset
