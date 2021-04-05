8602249e9870824f5f6efc0833569cb3d6889f9c
Eric Wiener <ericwiener3@gmail.com>
2021-04-05 13:05:00 -0400

    REFACTOR: changed WeatherNet to be a pl.LightningModule
    Also added a train.ipynb notebook to run overfitting and training

--- src/pcd_de_noising/model/weathernet.py
+++ src/pcd_de_noising/model/weathernet.py
@@ -1,11 +1,14 @@
+import pytorch_lightning as pl
 import torch
 import torch.hub as hub
 import torch.nn as nn
 import torch.nn.functional as F
+from torchmetrics import F1, HammingDistance
+
 from .lilanet import LiLaBlock
 
 
-class WeatherNet(nn.Module):
+class WeatherNet(pl.LightningModule):
     """
     Implements WeatherNet model from
     `"CNN-based Lidar Point Cloud De-Noising in Adverse Weather"
@@ -16,8 +19,7 @@ class WeatherNet(nn.Module):
     """
 
     def __init__(self, num_classes=3):
-        super(WeatherNet, self).__init__()
-
+        super().__init__()
         self.lila1 = LiLaBlock(2, 96, modified=True)
         self.lila2 = LiLaBlock(96, 128, modified=True)
         self.lila3 = LiLaBlock(128, 256, modified=True)
@@ -25,6 +27,12 @@ class WeatherNet(nn.Module):
         self.dropout = nn.Dropout2d()
         self.lila5 = LiLaBlock(256, 128, modified=True)
         self.classifier = nn.Conv2d(128, num_classes, kernel_size=1)
+
+        # Metrics
+        # self.train_f1 = F1(num_classes)
+        # self.train_hamming = HammingDistance()
+        # self.val_f1 = F1(num_classes)
+        # self.val_hamming = HammingDistance()
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -56,6 +64,69 @@ class WeatherNet(nn.Module):
         x = self.classifier(x)
 
         return x
+
+    def predict(self, distance, reflectivity):
+        """Run inference
+
+        Args:
+            distance (torch.tensor): distance tensor of dim [B, 1, 32, 400]
+            reflectivity (torch.tensor): reflectivity tensor of dim [B, 1, 32, 400]
+
+        Returns:
+            torch.tensor: predictions of dim [B, 32, 400]
+        """
+
+        with torch.no_grad():
+            # logits is [B, 4 (num classes), 32, 400]
+            logits = self(distance, reflectivity)
+
+            # predictions is [B, 32, 400]
+            predictions = torch.argmax(logits, dim=1)
+
+        return predictions
+
+    def shared_step(self, distance, reflectivity, labels):
+        # logits is [B, 4 (num classes), 32, 400]
+        logits = self(distance, reflectivity)
+
+        # labels is [B, 32, 400]
+        loss = F.cross_entropy(logits, labels)
+
+        # predictions is [B, 32, 400]
+        predictions = torch.argmax(logits, dim=1)
+
+        return loss, logits, predictions
+
+    def training_step(self, batch, batch_idx):
+        distance, reflectivity, labels = batch
+        loss, logits, predictions = self.shared_step(distance, reflectivity, labels)
+
+        self.log("train_loss", loss)
+
+        # Log Metrics
+        # self.train_f1(predictions, labels)
+        # self.log("train_f1", self.train_f1, on_step=False, on_epoch=True)
+
+        # self.train_hamming(logits, labels)
+        # self.log("train_hamming", self.train_hamming, on_step=False, on_epoch=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        distance, reflectivity, labels = batch
+        loss, logits, predictions = self.shared_step(distance, reflectivity, labels)
+
+        self.log("val_loss", loss)
+
+        # self.val_f1(predictions, labels)
+        # self.log("val_f1", self.val_f1, on_step=False, on_epoch=True)
+
+        # self.val_hamming(logits, labels)
+        # self.log("val_hamming", self.val_hamming, on_step=False, on_epoch=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), betas=(0.9, 0.999), eps=1e-8)
+        return optimizer
 
 
 if __name__ == "__main__":
